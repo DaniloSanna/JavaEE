397800722bf990efaf348d5cf59d52efd220c45d
Danilo Sanna <danilosanna@msn.com>
2021-12-08 20:04:36 -0300

    DataBase Connection
    Simple connection with MySQL Database.

---
+++ Basic_HTML/src/main/java/connection/SingleConnectionDataBase.java
@@ -0,0 +1,41 @@
+package connection;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+
+public class SingleConnectionDataBase {
+	private static String url = "jdbc:mysql://localhost:3306/java_ee?autoReconnect=true";
+	private static String user = "root";
+	private static String pass = "root";
+	private static Connection connection = null;
+
+	static {
+		toConcect();
+	}
+
+	public SingleConnectionDataBase() {
+		toConcect();
+	}
+
+	private static void toConcect() {
+		try {
+			if (connection == null) {
+				// Carrega driver de conexão do BD
+				Class.forName("com.mysql.jdbc.Driver");
+				connection = DriverManager.getConnection(url, user, pass);
+
+				// Não efetuar alterações sem aprovação direta.
+				connection.setAutoCommit(false);
+
+			}
+		} catch (Exception e) {
+			// Mostrar qualquer erro.
+			e.printStackTrace();
+		}
+	}
+
+	public static Connection getConnection() {
+		return connection;
+	}
+
+}
--- Basic_HTML/src/main/java/filter/FilterAutenticacao.java
+++ Basic_HTML/src/main/java/filter/FilterAutenticacao.java
@@ -1,7 +1,10 @@
 package filter;
 
 import java.io.IOException;
+import java.sql.Connection;
+import java.sql.SQLException;
 
+import connection.SingleConnectionDataBase;
 import jakarta.servlet.Filter;
 import jakarta.servlet.FilterChain;
 import jakarta.servlet.FilterConfig;
@@ -14,46 +17,75 @@ import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpSession;
 
 //Intercepta todas as requisições que vierem do projeto ou mapeamento
-@WebFilter(urlPatterns = { "/principal/*" }) 
+@WebFilter(urlPatterns = { "/principal/*" })
 
 public class FilterAutenticacao implements Filter {
+
+	private static Connection connection;
 
 	public FilterAutenticacao() {
 	}
 
 	// Encerra os processos quando o servidor for interrompida.
 	public void destroy() {
+		try {
+			connection.close();
+		} catch (SQLException e) {
+			System.out.println("DataBase -> Close Error:");
+			e.printStackTrace();
+		}
 	}
 
 	// Intercepta todas as requisições do projeto.
 	public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain)
 			throws IOException, ServletException {
 		
-		HttpServletRequest req = (HttpServletRequest) request;
-		HttpSession session = req.getSession();
-		String usuarioLogado = (String) session.getAttribute("login");
+		try {
+			HttpServletRequest req = (HttpServletRequest) request;
+			HttpSession session = req.getSession();
+			String usuarioLogado = (String) session.getAttribute("login");
 
-		// URL que esta sendo acessada.
-		String urlParaAtutenticar = req.getServletPath();
+			// URL que esta sendo acessada.
+			String urlParaAtutenticar = req.getServletPath();
 
-		// Validar se esta logado, caso não, direcione para tela de login.
-		if (usuarioLogado == null
-				&& !urlParaAtutenticar.contains("/principal/ServLetLogin")) {
+			// Validar se esta logado, caso não, direcione para tela de login.
+			if (usuarioLogado == null && !urlParaAtutenticar.contains("/principal/ServLetLogin")) {
 
-			RequestDispatcher redireciona = request.getRequestDispatcher("/index.jsp?url=" + urlParaAtutenticar);
-			request.setAttribute("msg", "Please, login first");
-			redireciona.forward(request, response);
+				RequestDispatcher redireciona = request.getRequestDispatcher("/index.jsp?url=" + urlParaAtutenticar);
+				request.setAttribute("msg", "Please, login first");
+				redireciona.forward(request, response);
 
-			// Para a execução e redireciona para login.
-			return;
-		} else {
-			chain.doFilter(request, response);
+				// Para a execução e redireciona para login.
+				return;
+			} else {
+				chain.doFilter(request, response);
+			}
+
+		}catch (Exception e) {
+			System.out.println("Unexpected Error:");
+			e.printStackTrace();
+			try {
+				connection.rollback();
+			} catch (SQLException e1) {
+				System.out.println("RollBack Error:");
+				e1.printStackTrace();
+			}
 		}
-
+		
+		//Se tudo funcionar.
+		try {
+			System.out.println("Tudo funcionou!");
+			connection.commit();
+		} catch (SQLException e) {
+			System.out.println("Commit Error:");
+			e.printStackTrace();
+		}
 	}
 
 	// Inicia os processos e recursos quando o servidor carrega o projeto
 	public void init(FilterConfig fConfig) throws ServletException {
+		connection = SingleConnectionDataBase.getConnection();
+
 	}
 
 }
